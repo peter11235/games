b3c769d1a7204b140a6467cdb471f96723ece11b
unknown <ptraunfeld@carpenterpc-09.fs.elon.edu>
2014-09-11 11:36:30 -0400

    Added jumping, climbing, and gravity

--- GameOne/GameOne/GameOne/GlitchPlayer.cs
+++ GameOne/GameOne/GameOne/GlitchPlayer.cs
@@ -15,8 +15,10 @@ namespace GameOne
 {
     class GlitchPlayer : UserControlledSprite
     {
+        float gravity = 3;
+        
         // state pattern
-        const int numberStates = 4;
+        const int numberStates = 7;
         enum GlitchPlayerState
         {
             Walking,
@@ -43,14 +45,31 @@ namespace GameOne
             states = new AbstractState[numberStates];
             states[(Int32)GlitchPlayerState.Walking] = new WalkingState(this);
             states[(Int32)GlitchPlayerState.Sleeping] = new SleepingState(this);
+            states[(Int32)GlitchPlayerState.Jumping] = new JumpingState(this);
+            states[(Int32)GlitchPlayerState.Climbing] = new ClimbingState(this);
 
             // start in Walking state
             switchState(GlitchPlayerState.Walking);
+            
+            
         }
 
         public override void Update(GameTime gameTime, Rectangle clientBounds)
         {
+            float bottom = clientBounds.Bottom;
+            if (currentState == GlitchPlayerState.Walking || currentState == GlitchPlayerState.Jumping)
+            {
+                if (this.position.Y < bottom+15)
+                {
+                    this.position.Y += gravity;
+                }
+            }
             states[(Int32)currentState].Update(gameTime, clientBounds);
+            if (GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.A) || Keyboard.GetState().IsKeyDown(Keys.Space)) //jump
+            {
+                switchState(GlitchPlayerState.Jumping);
+            }
+            
             base.Update(gameTime, clientBounds);
         }
 
@@ -94,7 +113,7 @@ namespace GameOne
             public override void Update(GameTime gameTime, Rectangle clientBounds)
             {
                 // pause animation if the sprite is not moving
-                if (player.direction.X == 0)
+                if (player.direction.X == 0 && player.direction.Y == 0)
                 {
                     player.pauseAnimation = true;
                     player.currentFrame = stillFrame; // standing frame
@@ -104,7 +123,10 @@ namespace GameOne
                     timeSinceLastMove = 0;
                     player.pauseAnimation = false;
                 }
-           
+                if (player.direction.X == 0 && player.direction.Y != 0)
+                {
+                    player.switchState(GlitchPlayerState.Climbing);
+                }
                 // transition to sleep state?
                 timeSinceLastMove += gameTime.ElapsedGameTime.Milliseconds;
                 if (timeSinceLastMove > timeForSleep)
@@ -128,6 +150,8 @@ namespace GameOne
 
             public override void Update(GameTime gameTime, Rectangle clientBounds)
             {
+
+
                 if (fallingToSleep)
                 {
                     sleepingPosition = player.position;
@@ -148,10 +172,10 @@ namespace GameOne
         }
 
 
-        /* Sleeping State */
+        /* Jumping State */
         private class JumpingState : AbstractState
         {
-           
+            Point halfway = new Point(15, 2);
             public JumpingState(GlitchPlayer player)
                 : base(player)
             {
@@ -159,11 +183,47 @@ namespace GameOne
 
             public override void Update(GameTime gameTime, Rectangle clientBounds)
             {
+
                 player.pauseAnimation = false;
+                player.position.X += 2*player.direction.X;
+
+                if (player.currentFrame.X < 15 && player.currentFrame.Y == 2)
+                {
+                    player.position.Y -= 5;
+                }
+
                 if (player.currentFrame == player.spriteSheet.currentSegment.endFrame)
                 {
+                    player.switchState(GlitchPlayerState.Walking); //start walking
+                }
+            }
+        }
+
+        /* Climbing State */
+        private class ClimbingState : AbstractState
+        {
+            Point startFrame;
+            public ClimbingState(GlitchPlayer player)
+                : base(player)
+            {
+                startFrame = new Point(0, 1);
+            }
+
+            public override void Update(GameTime gameTime, Rectangle clientBounds)
+            {
+                player.pauseAnimation = false;
+
+                if (player.direction.X == 0 && player.direction.Y == 0)
+                {
+                    player.pauseAnimation = true;
+                }
+                if (player.direction.X != 0) //if player starts moving horizontally
+                {
                     player.switchState(GlitchPlayerState.Walking);
-                    player.currentFrame = new Point(14, 0);  // start standing still
+                }
+                if (player.currentFrame == player.spriteSheet.currentSegment.endFrame)
+                {
+                    player.currentFrame = startFrame; //keep climbing until X position changes
                 }
             }
         }
